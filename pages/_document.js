1bc10245ba51f94a6a9cdb02e6531788242176c5
saadwafra <saad@wafra.net>
2026-02-18 16:53:15 +0300

    faviconchange

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" type="image/png" href="/images/logo/arbex.png" />
+          <link rel="shortcut icon" type="image/png" href="/images/logo/arbex.png" />
+          <link rel="apple-touch-icon" href="/images/logo/arbex.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&family=Playfair+Display:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
